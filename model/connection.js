762dc4c744bbd2045fab04ededec27aafe08e851
Sachin kumar <130838646+Sachin8757@users.noreply.github.com>
2025-04-04 12:45:23 +0530

    Update connection.js

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose')
 const {Schema}=mongoose.Schema;
 const dotenv=require('dotenv').config();
- const url=process.env.ATLASDB_URL;
+ const url=mongodb+srv://sachin:8757887103@shopdata.shpwbu2.mongodb.net/?retryWrites=true&w=majority&appName=shopdata;
+ // const url=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("db connected !");
 }).catch((err)=>{
